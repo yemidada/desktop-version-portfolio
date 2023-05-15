9c6f1456e5bd1b2e6510dfe42422130a7d439d59
yemidada <yemidada@gmail.com>
2023-05-15 14:38:29 +0100

    Updated script file

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,20 @@
-
-document.querySelector('#mobile-menu-btn').addEventListener('click', function () {
-    var d = document.getElementsByClassName("popup-menu");
-    for (var i = 0; i < d.length; i++) {
-        d[i].className += " popup-menu-active";
-    }
+document.querySelector('#mobile-menu-btn').addEventListener('click', () => {
+  const d = document.getElementsByClassName('popup-menu');
+  for (let i = 0; i < d.length; i += 1) {
+    d[i].className += ' popup-menu-active';
+  }
 });
 
-document.querySelector('#popup-close-btn').addEventListener('click', function () {
-    var d = document.getElementsByClassName("popup-menu");
-    for (var i = 0; i < d.length; i++) {
-        d[i].classList.remove("popup-menu-active");
-    }
+document.querySelector('#popup-close-btn').addEventListener('click', () => {
+  const d = document.getElementsByClassName('popup-menu');
+  for (let i = 0; i < d.length; i += 1) {
+    d[i].classList.remove('popup-menu-active');
+  }
 });
 
-document.querySelector('.popup-menu-item a').addEventListener('click', function () {
-    var d = document.getElementsByClassName("popup-menu");
-    for (var i = 0; i < d.length; i++) {
-        d[i].classList.remove("popup-menu-active");
-    }
+document.querySelector('.popup-menu ul').addEventListener('click', () => {
+  const d = document.getElementsByClassName('popup-menu');
+  for (let i = 0; i < d.length; i += 1) {
+    d[i].classList.remove('popup-menu-active');
+  }
 });
